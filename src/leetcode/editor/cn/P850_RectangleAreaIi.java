114e7d7d59e03bc2994f77bf5010fb52605fecd6
西山对寂寥 <1781287641@qq.com>
2022-09-19 14:54:07 +0800

    22/09/19

---
+++ src/leetcode/editor/cn/P850_RectangleAreaIi.java
@@ -0,0 +1,22 @@
+/**
+ * @Title: 矩形面积 II 
+ * @Author: Neo
+ * @Date: 2022-09-16 23:53:55
+ */
+package leetcode.editor.cn;
+public class P850_RectangleAreaIi{
+	 public static void main(String[] args) {
+	 	 //测试代码
+	 	 Solution solution = new P850_RectangleAreaIi().new Solution();
+	 }
+	 
+//力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int rectangleArea(int[][] rectangles) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}

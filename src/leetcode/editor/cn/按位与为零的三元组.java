faf319ff9f24e1b12d4f7de9325359571370538d
西山对寂寥 <1781287641@qq.com>
2023-03-04 15:41:34 +0800

    23/3/4,位运算、数组、哈希表

---
+++ src/leetcode/editor/cn/按位与为零的三元组.java
@@ -0,0 +1,36 @@
+/**
+ * @Title: 982.按位与为零的三元组
+ * @TitleSlug: triplesWithBitwiseAndEqualToZero
+ * @Author: Neo
+ * @Date: 2023-03-04 15:09:39
+ */
+package leetcode.editor.cn;
+
+public class 按位与为零的三元组 {
+    public static void main(String[] args) {
+        //测试代码
+        Solution solution = new 按位与为零的三元组().new Solution();
+    }
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int countTriplets(int[] nums) {
+        int[] cnt = new int[1 << 16];
+        for (int i : nums) {
+            for (int j : nums) {
+                cnt[i & j]++;
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < 1 << 16; i++) {
+            for (int k : nums) {
+                if ((i & k) == 0) {
+                    ans += cnt[i];
+                }
+            }
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)

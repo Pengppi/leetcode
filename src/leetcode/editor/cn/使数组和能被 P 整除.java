38fc8ce84fa18afd3dea152e0038c46de59f9206
西山对寂寥 <1781287641@qq.com>
2023-03-10 14:20:42 +0800

    23/3/10,同余，哈希表

---
+++ src/leetcode/editor/cn/使数组和能被 P 整除.java
@@ -0,0 +1,41 @@
+/**
+ * @Title: 1590.使数组和能被 P 整除
+ * @TitleSlug: makeSumDivisibleByP
+ * @Author: Neo
+ * @Date: 2023-03-10 12:41:01
+ */
+package leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 使数组和能被 P 整除 {
+    public static void main(String[] args) {
+        //测试代码
+        Solution solution = new 使数组和能被 P 整除 ().new Solution();
+    }
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int minSubarray(int[] nums, int p) {
+        int n = nums.length;
+        int[] sum = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            sum[i + 1] = (sum[i] + nums[i]) % p;
+        }
+        int t = sum[n];
+        if (t == 0) {
+            return 0;
+        }
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int res = n;
+        for (int i = 0; i <= n; i++) {
+            map.put(sum[i], i);
+            int j = map.getOrDefault((sum[i] - t + p) % p, -n);
+            res = Math.min(res, i - j);
+        }
+        return res >= n ? -1 : res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)

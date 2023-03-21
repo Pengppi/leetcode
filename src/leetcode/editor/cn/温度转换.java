e5866ae07b6621ea34566661f72f1ad25f3156a3
西山对寂寥 <1781287641@qq.com>
2023-03-21 15:02:17 +0800

    23/3/21,模拟

---
+++ src/leetcode/editor/cn/温度转换.java
@@ -0,0 +1,24 @@
+/**
+ * @Title: 2469.温度转换
+ * @TitleSlug: convertTheTemperature
+ * @Author: Neo
+ * @Date: 2023-03-21 14:46:48
+ */
+package leetcode.editor.cn;
+
+public class 温度转换 {
+    public static void main(String[] args) {
+        //测试代码
+        Solution solution = new 温度转换().new Solution();
+    }
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        double kelvin = celsius + 273.15;
+        double fahrenheit = celsius * 1.80 + 32.00;
+        return new double[]{kelvin, fahrenheit};
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
